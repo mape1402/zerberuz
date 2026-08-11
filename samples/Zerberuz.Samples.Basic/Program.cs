3abe120ba77d51f1e3dbe206d3e8d53f513cda1b
Mario Perez <mperez@elysiumcoding.com>
2026-08-11 10:58:44 -0600

    Prepare sample for analyzer diagnostics

--- samples/Zerberuz.Samples.Basic/Program.cs
+++ samples/Zerberuz.Samples.Basic/Program.cs
@@ -1,1 +1,25 @@
 Console.WriteLine("Zerberuz sample shell");
+
+var repository = new SampleRepository();
+Console.WriteLine(repository.GetType().Name);
+
+public interface Repository
+{
+    string FindById(string id);
+}
+
+public sealed class SampleRepository : Repository
+{
+    public string FindById(string id)
+    {
+        return id;
+    }
+}
+
+public sealed class OrderService
+{
+    public string GetStatus()
+    {
+        return "Created";
+    }
+}
